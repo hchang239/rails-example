3dc6b0b65fed751d31d723be316f63cfe46f3afc
Kyle Chang <hchang239@berkeley.edu>
2016-01-23 17:14:29 -0800

    change spec to serve root

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
-  it { should route(:get, 'static_pages/home').to(action: :home) }
+  it { should route(:get, '/').to(action: :home) }
   it { should route(:get, 'static_pages/help').to(action: :help) }
   it { should route(:get, 'static_pages/about').to(action: :about) }
 
